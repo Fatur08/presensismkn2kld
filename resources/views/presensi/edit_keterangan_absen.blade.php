9432aa798483360cd77631ca8923be5fdd2f6ff9
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-24 10:12:51 +0700

    update

--- resources/views/presensi/edit_keterangan_absen.blade.php
+++ resources/views/presensi/edit_keterangan_absen.blade.php
@@ -26,8 +26,8 @@
     </div>
     <div class="row mt-3">
         <div class="col-12">
+            <h4>Keterangan</h4>
             <div class="input-icon">
-                <h4>Keterangan</h4>
                 <span class="input-icon-addon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
                         viewBox="0 0 24 24" fill="none" stroke="currentColor" 
