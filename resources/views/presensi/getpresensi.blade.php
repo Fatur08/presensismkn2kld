fa1f3e75079efdd7e520cb7a2568c03bc3500a77
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-27 10:40:55 +0700

    upload

--- resources/views/presensi/getpresensi.blade.php
+++ resources/views/presensi/getpresensi.blade.php
@@ -104,6 +104,21 @@ function selisih($jam_batas, $jam_masuk)
                         </svg>
                         Edit
                     </a>
+                @else
+                    <a href="#"
+                       class="btn btn-secondary edit_keterangan_absen"
+                       id       = "{{ $d->id }}"
+                       nisn     = "{{ $d->nisn }}"
+                       tanggal  = "{{ $tanggal }}"
+                       style="font-size:8pt; padding:2px 6px; height:auto; line-height:1;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-article">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+                            <path d="M7 8h10" />
+                            <path d="M7 12h10" />
+                            <path d="M7 16h10" />
+                        </svg>
+                        Bukti
+                    </a>
                 @endif
             </td>
         </tr>
