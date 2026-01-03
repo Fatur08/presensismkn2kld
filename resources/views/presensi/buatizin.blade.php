dd13cf22da5c9dd56a10fd54d36edda01ef38082
Fatur08 <dhutafaturrahman@gmail.com>
2026-01-03 14:51:21 +0700

    update

--- resources/views/presensi/buatizin.blade.php
+++ resources/views/presensi/buatizin.blade.php
@@ -23,43 +23,39 @@
 <!--- * App Header -->
 @endsection
 @section('content')
-<div class="row" style="margin-top: 70px;">
+<form action="/presensi/storeizin" method="POST" id="frmIzin">
+    @csrf
     <div class="col">
-        <form action="/presensi/storeizin" method="POST" id="frmIzin">
-            @csrf
-            <div class="col">
-                <div class="form-group boxed">
-                    <h4>Masukkan Tanggal</h4>
-                    <div class="input-wrapper">
-                        <input type="text" id="tgl_izin" name="tgl_izin" class="form-control datepicker" placeholder="Tanggal">
-                    </div>
-                </div>
-                <div class="form-group boxed">
-                    <select name="status" id="status" class="form-control">
-                        <option value="">Pilih Status</option>
-                        <option value="i">Izin</option>
-                        <option value="s">Sakit</option>
-                    </select>
-                </div>
-                <div class="form-group boxed">
-                    <h4>Masukkan Keterangan</h4>
-                    <textarea name="keterangan" id="keterangan" cols="30" rows="5" class="form-control" placeholder="Keterangan"></textarea>
-                </div>
-                <div class="form-group boxed">
-                    <h4>Masukkan Bukti Izin / Sakit</h4>
-                    <div class="input-wrapper">
-                        <input type="file" name="bukti_izin" id="bukti_izin" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group boxed">
-                    <div class="input-wrapper">
-                        <button class="btn btn-primary w-100">Kirim</button>
-                    </div>
-                </div>
+        <div class="form-group boxed">
+            <h4>Masukkan Tanggal</h4>
+            <div class="input-wrapper">
+                <input type="text" id="tgl_izin" name="tgl_izin" class="form-control datepicker" placeholder="Tanggal">
             </div>
-        </form>
+        </div>
+        <div class="form-group boxed">
+            <select name="status" id="status" class="form-control">
+                <option value="">Pilih Status</option>
+                <option value="i">Izin</option>
+                <option value="s">Sakit</option>
+            </select>
+        </div>
+        <div class="form-group boxed">
+            <h4>Masukkan Keterangan</h4>
+            <textarea name="keterangan" id="keterangan" cols="30" rows="5" class="form-control" placeholder="Keterangan"></textarea>
+        </div>
+        <div class="form-group boxed">
+            <h4>Masukkan Bukti Izin / Sakit</h4>
+            <div class="input-wrapper">
+                <input type="file" name="bukti_izin" id="bukti_izin" class="form-control">
+            </div>
+        </div>
+        <div class="form-group boxed">
+            <div class="input-wrapper">
+                <button class="btn btn-primary w-100">Kirim</button>
+            </div>
+        </div>
     </div>
-</div>
+</form>
 @endsection
 @push('myscript')
 <script>
