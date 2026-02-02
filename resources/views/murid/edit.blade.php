60023a149622e803f9a3e7d12554e54b810d134d
Fatur08 <dhutafaturrahman@gmail.com>
2026-02-03 00:24:50 +0700

    update

--- resources/views/murid/edit.blade.php
+++ resources/views/murid/edit.blade.php
@@ -9,7 +9,7 @@
                   <!-- Download SVG icon from http://tabler-icons.io/i/user -->
                   <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-barcode"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7v-1a2 2 0 0 1 2 -2h2" /><path d="M4 17v1a2 2 0 0 0 2 2h2" /><path d="M16 4h2a2 2 0 0 1 2 2v1" /><path d="M16 20h2a2 2 0 0 0 2 -2v-1" /><path d="M5 11h1v2h-1z" /><path d="M10 11l0 2" /><path d="M14 11h1v2h-1z" /><path d="M19 11l0 2" /></svg>
                 </span>
-                <input type="hidden" name="nisn_lama" value="{{ $murid->nisn }}">
+                <input type="hidden" id="nisn_lama" name="nisn_lama" value="{{ $murid->nisn }}">
                 <input
                     type="text"
                     value="{{ $murid->nisn }}"
@@ -33,7 +33,7 @@
                   <!-- Download SVG icon from http://tabler-icons.io/i/user -->
                   <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
                 </span>
-                <input type="text" value="{{ $murid->nama_lengkap}}" id="nama_lengkap" class="form-control" name="nama_lengkap" placeholder="Nama Lengkap">
+                <input type="text" value="{{ $murid->nama_lengkap}}" id="nama_murid" name="nama_murid" class="form-control" placeholder="Nama Lengkap">
               </div>
         </div>
     </div>
@@ -115,7 +115,7 @@
     $(function(){
         $("#FormEditMurid").submit(function(){
             var nisn_baru = $("#nisn_baru").val();
-            var nama_lengkap = $("#nama_lengkap").val();
+            var nama_murid = $("#nama_murid").val();
             var kelas = $("#kelas").val();
             var kode_jurusan = $("#kode_jurusan").val();
             var no_hp = $("#no_hp").val();
