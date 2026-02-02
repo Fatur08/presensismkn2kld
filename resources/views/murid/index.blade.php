3e493957ddea60beb4294c7e06936f06114a39a9
Fatur08 <dhutafaturrahman@gmail.com>
2026-02-02 23:14:16 +0700

    update

--- resources/views/murid/index.blade.php
+++ resources/views/murid/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.admin.tabler')
 @section('content')
+<style>
+    .input-icon {
+        position: relative;
+    }
+
+    .input-icon-addon {
+        display: flex;
+        align-items: center;   /* ⬅️ bikin icon benar-benar center vertikal */
+        justify-content: center;
+    }
+</style>
 <div class="page-header d-print-none">
     <div class="container-xl">
         <div class="row align-items-center justify-content-between">
@@ -224,7 +235,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                          viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                          stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                         class="icon icon-tabler icons-tabler-outline icon-tabler-phone">
+                                         class="icon icon-tabler icon-tabler-phone">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                         <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4
                                                  a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"/>
@@ -243,7 +254,7 @@
                                     maxlength="15"
                                     oninput="this.value = this.value.replace(/[^0-9]/g, '')"
                                 >
-                            
+                    
                                 <small class="text-muted">
                                     Nomor HP hanya boleh angka (contoh: 08xxxxxxxxxx)
                                 </small>
